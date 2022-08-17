fd4e6b1139b94cd2e161435db2563a8420fbe5b2
MilkBeeno <milkbeeno@gmail.com>
2022-08-17 11:02:16 +0800

    适配 Android 12 问题

--- vpn/src/main/java/com/freetech/vpn/logic/Scheduler.java
+++ vpn/src/main/java/com/freetech/vpn/logic/Scheduler.java
@@ -34,143 +34,132 @@ import java.util.PriorityQueue;
 import java.util.UUID;
 
 @Keep
-public class Scheduler extends BroadcastReceiver
-{
-	private final String EXECUTE_JOB = "com.freetech.vpn.android.Scheduler.EXECUTE_JOB";
-	private final Context mContext;
-	private final AlarmManager mManager;
-	private final PriorityQueue<ScheduledJob> mJobs;
+public class Scheduler extends BroadcastReceiver {
+    private final String EXECUTE_JOB = "com.freetech.vpn.android.Scheduler.EXECUTE_JOB";
+    private final Context mContext;
+    private final AlarmManager mManager;
+    private final PriorityQueue<ScheduledJob> mJobs;
 
-	public Scheduler(Context context)
-	{
-		mContext = context;
-		mManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
-		mJobs = new PriorityQueue<>();
+    public Scheduler(Context context) {
+        mContext = context;
+        mManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        mJobs = new PriorityQueue<>();
 
-		IntentFilter filter = new IntentFilter();
-		filter.addAction(EXECUTE_JOB);
-		mContext.registerReceiver(this, filter);
-	}
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(EXECUTE_JOB);
+        mContext.registerReceiver(this, filter);
+    }
 
-	/**
-	 * Remove all pending jobs and unregister the receiver.
-	 * Called via JNI.
-	 */
-	public void Terminate()
-	{
-		synchronized (this)
-		{
-			mJobs.clear();
-		}
-		mManager.cancel(createIntent());
-		mContext.unregisterReceiver(this);
-	}
+    /**
+     * Remove all pending jobs and unregister the receiver.
+     * Called via JNI.
+     */
+    public void Terminate() {
+        synchronized (this) {
+            mJobs.clear();
+        }
+        mManager.cancel(createIntent());
+        mContext.unregisterReceiver(this);
+    }
 
-	/**
-	 * Allocate a job ID. Called via JNI.
-	 *
-	 * @return random ID for a new job
-	 */
-	public String allocateId()
-	{
-		return UUID.randomUUID().toString();
-	}
+    /**
+     * Allocate a job ID. Called via JNI.
+     *
+     * @return random ID for a new job
+     */
+    public String allocateId() {
+        return UUID.randomUUID().toString();
+    }
 
-	/**
-	 * Create a pending intent to execute a job.
-	 *
-	 * @return pending intent
-	 */
-	private PendingIntent createIntent()
-	{
-		/* using component/class doesn't work with dynamic broadcast receivers */
-		Intent intent = new Intent(EXECUTE_JOB);
-		intent.setPackage(mContext.getPackageName());
+    /**
+     * Create a pending intent to execute a job.
+     *
+     * @return pending intent
+     */
+    private PendingIntent createIntent() {
+        /* using component/class doesn't work with dynamic broadcast receivers */
+        Intent intent = new Intent(EXECUTE_JOB);
+        intent.setPackage(mContext.getPackageName());
         intent.setComponent(new ComponentName(mContext, Scheduler.class));
         int flag = Build.VERSION.SDK_INT >= 23 ? FLAG_IMMUTABLE : 0;
         return PendingIntent.getBroadcast(mContext, 0, intent, flag);
-	}
+    }
 
-	/**
-	 * Schedule executing a job in the future.
-	 * Called via JNI from different threads.
-	 *
-	 * @param id job ID
-	 * @param ms delta in milliseconds when the job should be executed
-	 */
-	@RequiresApi(api = Build.VERSION_CODES.M)
-	public void scheduleJob(String id, long ms)
-	{
-		synchronized (this)
-		{
-			ScheduledJob job = new ScheduledJob(id, System.currentTimeMillis() + ms);
-			mJobs.add(job);
+    /**
+     * Schedule executing a job in the future.
+     * Called via JNI from different threads.
+     *
+     * @param id job ID
+     * @param ms delta in milliseconds when the job should be executed
+     */
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void scheduleJob(String id, long ms) {
+        synchronized (this) {
+            ScheduledJob job = new ScheduledJob(id, System.currentTimeMillis() + ms);
+            mJobs.add(job);
 
-			if (job == mJobs.peek())
-			{	/* update the alarm if the job has to be executed before all others */
-				PendingIntent pending = createIntent();
-				mManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, job.Time, pending);
-			}
-		}
-	}
+            if (job == mJobs.peek()) {    /* update the alarm if the job has to be executed before all others */
+                PendingIntent pending = createIntent();
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                    boolean isHave = mManager.canScheduleExactAlarms();
+                    if (isHave) {
+                        mManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, job.Time, pending);
+                    }
+                } else {
+                    mManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, job.Time, pending);
+                }
+            }
+        }
+    }
 
-	@RequiresApi(api = Build.VERSION_CODES.M)
-	@Override
-	public void onReceive(Context context, Intent intent)
-	{
-		ArrayList<ScheduledJob> jobs = new ArrayList<>();
-		long now = System.currentTimeMillis();
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        ArrayList<ScheduledJob> jobs = new ArrayList<>();
+        long now = System.currentTimeMillis();
 
-		synchronized (this)
-		{
-			ScheduledJob job = mJobs.peek();
-			while (job != null)
-			{
-				if (job.Time > now)
-				{
-					break;
-				}
-				jobs.add(mJobs.remove());
-				job = mJobs.peek();
-			}
-			if (job != null)
-			{
-				PendingIntent pending = createIntent();
-				mManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, job.Time, pending);
-			}
-		}
+        synchronized (this) {
+            ScheduledJob job = mJobs.peek();
+            while (job != null) {
+                if (job.Time > now) {
+                    break;
+                }
+                jobs.add(mJobs.remove());
+                job = mJobs.peek();
+            }
+            if (job != null) {
+                PendingIntent pending = createIntent();
+                mManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, job.Time, pending);
+            }
+        }
 
-		for (ScheduledJob job : jobs)
-		{
-			executeJob(job.Id);
-		}
-	}
+        for (ScheduledJob job : jobs) {
+            executeJob(job.Id);
+        }
+    }
 
-	/**
-	 * Execute the job with the given ID.
-	 *
-	 * @param id job ID
-	 */
-	public native void executeJob(String id);
+    /**
+     * Execute the job with the given ID.
+     *
+     * @param id job ID
+     */
+    public native void executeJob(String id);
 
-	/**
-	 * Keep track of scheduled jobs.
-	 */
-	private static class ScheduledJob implements Comparable<ScheduledJob>
-	{
-		String Id;
-		long Time;
+    /**
+     * Keep track of scheduled jobs.
+     */
+    private static class ScheduledJob implements Comparable<ScheduledJob> {
+        String Id;
+        long Time;
 
-		ScheduledJob(String id, long time)
-		{
-			Id = id;
-			Time = time;
-		}
+        ScheduledJob(String id, long time) {
+            Id = id;
+            Time = time;
+        }
 
-		@Override
-		public int compareTo(ScheduledJob o)
-		{
-			return Long.compare(Time, o.Time);
-		}
-	}
+        @Override
+        public int compareTo(ScheduledJob o) {
+            return Long.compare(Time, o.Time);
+        }
+    }
 }

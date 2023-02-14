c23ac59c41ae1ee96dc3254418e577d26e3a7f53
MilkBeeno <milkbeeno@gmail.com>
2023-02-14 14:08:52 +0800

    隐私协议返回按钮问题

--- app/src/main/java/com/milk/global/ui/act/WebActivity.kt
+++ app/src/main/java/com/milk/global/ui/act/WebActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import com.milk.global.databinding.ActivityWebBinding
+import com.milk.simple.ktx.immersiveStatusBar
+import com.milk.simple.ktx.statusBarPadding
 
 class WebActivity : AbstractActivity() {
     private val binding by lazy { ActivityWebBinding.inflate(layoutInflater) }
@@ -14,6 +16,9 @@ class WebActivity : AbstractActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        immersiveStatusBar()
+        binding.ivBack.statusBarPadding()
+        binding.ivBack.setOnClickListener { finish() }
         binding.webView.loadUrl(url)
         binding.webView.webViewClient = object : WebViewClient() {
             @Deprecated("Deprecated in Java")

7e9232ebbc8f34fbac609d4592a3b1059ce6871e
MilkBeeno <milkbeeno@gmail.com>
2023-02-23 16:43:21 +0800

    vpn 屏蔽包名

--- app/src/main/java/com/milk/global/repository/AppRepository.kt
+++ app/src/main/java/com/milk/global/repository/AppRepository.kt
@@ -80,11 +80,11 @@ object AppRepository {
             val apiResult = apiResponse.data
             if (apiResponse.success && apiResult != null) {
                 shareAppUrl = apiResult.share_copy
-
-                VpnWhiteList.vpnList.clear()
-                val parts = apiResult.Shield_app_bundle.split("&")
-                parts.forEach { VpnWhiteList.vpnList.add(it) }
-
+                if (apiResult.Shield_app_bundle != "0") {
+                    VpnWhiteList.vpnList.clear()
+                    val parts = apiResult.Shield_app_bundle.split("&")
+                    parts.forEach { VpnWhiteList.vpnList.add(it) }
+                }
                 try {
                     showOpenAd = apiResult.openAd.toInt() == 0
                     showMainNativeAd = apiResult.mainNativeAd.toInt() == 0
